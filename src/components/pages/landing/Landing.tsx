940ad34dbb199c65631ab7784c01695210840a18
tranceyos2419 <31304661+tranceyos2419@users.noreply.github.com>
2020-03-11 11:01:19 +0900

    build: initialized css

---
+++ src/components/pages/landing/Landing.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+interface Props {}
+
+const Landing = (props: Props) => {
+  return (
+    <div>
+      <h3>Landing</h3>
+    </div>
+  );
+};
+
+export default Landing;
