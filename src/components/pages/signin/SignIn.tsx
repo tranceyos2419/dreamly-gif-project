f17e330929f2acfff7744d097179ed52ac7ad750
tranceyos2419 <31304661+tranceyos2419@users.noreply.github.com>
2020-03-11 12:57:50 +0900

    build: signup and signin pages were added

---
+++ src/components/pages/signin/SignIn.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+interface Props {}
+
+const SignIn = (props: Props) => {
+  return (
+    <div>
+      <h3>Sign In</h3>
+    </div>
+  );
+};
+
+export default SignIn;
---
+++ src/components/pages/signup/SignUp.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+interface Props {}
+
+const SignUp = (props: Props) => {
+  return (
+    <div>
+      <h3>Sign up</h3>
+    </div>
+  );
+};
+
+export default SignUp;
