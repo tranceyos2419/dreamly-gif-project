37d86233ac4b130354a85414e5ce593dc6b7c159
tranceyos2419 <31304661+tranceyos2419@users.noreply.github.com>
2020-03-11 11:11:17 +0900

    build: add RootWrapper to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
 import React from "react";
 import Routes from "./routes/Routes";
+import styled, { css } from "styled-components";
+
+const RootWrapper = styled.div(
+  ({ theme }) => css`
+    text-align: center;
+    margin: auto;
+    max-width: ${theme.range.screen.maxWidth};
+  `
+);
 
 function App() {
   return (
-    <>
+    <RootWrapper>
       <Routes />
-    </>
+    </RootWrapper>
   );
 }
 
--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -2,13 +2,14 @@ import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Counter from "../components/pages/counter/Counter";
 import FirebaseTest from "../components/pages/firebaseTest/FirebaseTest";
+import Landing from "../components/pages/landing/Landing";
 interface Props {}
 
 const Routes = (props: Props) => {
   return (
     <Router>
       <Switch>
-        <Route exact path="/" component={Counter} />
+        <Route exact path="/" component={Landing} />
         <Route exact path="/test" component={FirebaseTest} />
       </Switch>
     </Router>
