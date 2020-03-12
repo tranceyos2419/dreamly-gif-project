548bd3eb4577cf90387ba6287655330332e62522
tranceyos2419 <31304661+tranceyos2419@users.noreply.github.com>
2020-03-12 22:09:03 +0900

    build: redirect a user to landing page after a user logged out

--- src/components/pages/feed/Feed.tsx
+++ src/components/pages/feed/Feed.tsx
@@ -1,17 +1,19 @@
 import React from "react";
 import { useFirestore, useFirebase } from "react-redux-firebase";
 import { useToLandingIfNotAuthenticated } from "../../hooks/myCustomHooks";
+import { useHistory } from "react-router-dom";
 
 interface Props {}
 
 const Feed = (props: Props) => {
   const firebase = useFirebase();
+  const history = useHistory();
   //todo remove
   const logout = () => {
     firebase
       .auth()
       .signOut()
-      .then(() => alert("you are signed out"));
+      .then(() => history.push("/"));
   };
   useToLandingIfNotAuthenticated();
   return (
