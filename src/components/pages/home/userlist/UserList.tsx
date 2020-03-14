a92db6dc0d296d62159b00d3bfc9fa5e4d9231f5
tranceyos2419 <31304661+tranceyos2419@users.noreply.github.com>
2020-03-14 21:56:03 +0900

    build: passing data to User from UserList

--- src/components/pages/home/userlist/UserList.tsx
+++ src/components/pages/home/userlist/UserList.tsx
@@ -12,27 +12,22 @@ export interface IUser {
 
 const UserList = (props: Props) => {
   useFirestoreConnect({ collection: "users" });
+  let users: IUser[] = [];
   const state = useSelector((state: any) => state);
-  const [users, setUsers] = useState(null);
-  let values: any;
   const userObj = state.firestore.data.users as Object;
 
   if (userObj !== null && userObj !== undefined) {
-    values = Object.values(userObj) as IUser[];
+    users = Object.values(userObj) as IUser[];
   }
-
-  useEffect(() => {
-    setUsers(values);
-  }, values);
 
   return (
     <div>
       <h2>User list</h2>
-      {/* {users &&
-        users.map(user => {
+      {users &&
+        users.map((user: IUser) => {
           const { name, email } = user;
           return <User name={name} email={email} />;
-        })} */}
+        })}
     </div>
   );
 };
