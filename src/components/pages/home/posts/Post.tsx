c27de25270628b90a284ea91a63573ac4a6c968b
tranceyos2419 <31304661+tranceyos2419@users.noreply.github.com>
2020-03-19 20:40:35 +0900

    build: displayed the nubmer of likes of posts

--- src/components/pages/home/posts/Post.tsx
+++ src/components/pages/home/posts/Post.tsx
@@ -42,9 +42,19 @@ const ActionBar = styled.div(
     justify-content: space-between;
     padding: 0.2em 0.4em;
     border-bottom: 1px solid #ccc;
+    border: 1px solid black;
   `
 );
 
+const Likes = styled.p(
+  ({ theme }) => css`
+    margin-left: 0.2em;
+    margin-top: -5%;
+    display: inline-block;
+    color: black;
+    font-size: ${theme.size.font.regular};
+  `
+);
 interface WasUserLiked {
   wasUserLiked: boolean;
 }
@@ -90,11 +100,14 @@ const Post = (props: Props) => {
       />
       <StyledImg src={imgUrl} alt="gif" />
       <ActionBar>
-        <StyledIcon
-          wasUserLiked={wasUserLiked}
-          icon={faHeart}
-          onClick={() => handleLike()}
-        />
+        <div>
+          <StyledIcon
+            wasUserLiked={wasUserLiked}
+            icon={faHeart}
+            onClick={() => handleLike()}
+          />
+          <Likes>{likes.length}</Likes>
+        </div>
       </ActionBar>
 
       {type === EPostType.Inbox && (
